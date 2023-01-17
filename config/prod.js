f05c86ebcee79c104bbb06c1a9d040aff937272e
MauricioEin <mauricio.ein@gmail.com>
2023-01-17 13:22:32 +0200

    db update

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,9 @@
+require('dotenv').config()
+
+const dbName = process.env.DB_NAME
+const dbKey = process.env.DB_KEY
+
 module.exports = {
-  dbURL: 'mongodb+srv://MuseUp:museforever@cluster0.7bgfjlk.mongodb.net/?retryWrites=true&w=majority',
+  dbURL: `mongodb+srv://${dbName}:${dbKey}@cluster0.7bgfjlk.mongodb.net/?retryWrites=true&w=majority`,
   dbName : 'station_db'
 }
